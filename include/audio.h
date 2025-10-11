ebf5b169c833a94dfd055765fc5a1364921e21a0
Jacob Thompson <jacobalthompson@gmail.com>
2025-10-11 12:21:24 -0700

    fix audio playback issues
    fixes #8

--- include/audio.h
+++ include/audio.h
@@ -5,13 +5,14 @@
 #define TONE_FREQ 440 // A4 tone
 #define AMPLITUDE 1000 // volume of the tone
 #define SAMPLE_RATE 44100
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 512  // ~11.6ms at 44100Hz for responsive audio
 
 typedef struct audio {
     SDL_AudioDeviceID deviceId;
     SDL_AudioSpec spec;
     bool playing;
     bool poweredOn;
+    double phase;
 } audio;
 
 void my_audio_callback(void *userdata, Uint8 *stream, int len);
