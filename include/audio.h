138965dbc59bb5f8981fbacc314cd9e8575ae6b4
Jacob Thompson <jacobalthompson@gmail.com>
2025-10-12 03:14:18 -0700

    fix audio.h documentation

--- include/audio.h
+++ include/audio.h
@@ -18,10 +18,15 @@ typedef struct audio {
 
 /*
  * Audio callback function to generate a tone
+ * @param userdata Pointer to audio structure
+ * @param stream Pointer to the audio buffer to fill
+ * @param len Length of the audio buffer
  */
 void audioCallback(void *userdata, Uint8 *stream, int len);
 
 /*
  * Initialize the audio system
+ * @param audio Pointer to audio structure
+ * @return 0 on success, -1 on failure
  */
 int initAudio(audio *audio);
